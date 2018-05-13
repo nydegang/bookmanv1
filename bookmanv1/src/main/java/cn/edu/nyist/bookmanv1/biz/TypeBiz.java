ae3fa27c0a9158dec73b4a6501bec05802fbd88e
nydegang <149098880@qq.com>
2018-05-13 10:54:18 +0800

    修改了业务

--- bookmanv1/src/main/java/cn/edu/nyist/bookmanv1/biz/TypeBiz.java
+++ bookmanv1/src/main/java/cn/edu/nyist/bookmanv1/biz/TypeBiz.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import cn.edu.nyist.bookmanv1.vo.TypeVo;
 
+/**
+ * 
+ * @author 南阳德刚版权所有,企鹅号:2855128836<br>
+ *         2018年5月13日上午10:52:12<br>
+ *         说明:书籍类型业务层
+ */
 public interface TypeBiz {
 
 	List<TypeVo> findAllTypes();
